e1e4bf3e73453e9281fe47830448dec92e16ed80
reason77-ctrl <reasonregmi27@gmail.com>
2025-02-23 14:16:45 +0545

    flags in dropdown

--- currencyConverter/script.js
+++ currencyConverter/script.js
@@ -1,13 +1,17 @@
-const BASE_URL = "https://latest.currency-api.pages.dev/v1/currencies/eur.json";
+const BASE_URL = "https://latest.currency-api.pages.dev/v1/currencies/";
 
 const country_list =
   "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.min.json";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector("select[name='from']");
+const toCurr = document.querySelector("select[name='to']");
 
 const countryList = (async () => {
   let response = await fetch(country_list);
   let countryList = await response.json();
+
   for (const select of dropdowns) {
     for (currCode in countryList) {
       let newOption = document.createElement("option");
@@ -15,12 +19,39 @@ const countryList = (async () => {
         countryList[currCode]
       }`;
       newOption.value = currCode;
-      select.append(newOption);
       if (select.name === "from" && currCode === "usd") {
         newOption.selected = "selected";
       } else if (select.name === "to" && currCode === "npr") {
         newOption.selected = "selected";
       }
+      select.append(newOption);
     }
+    select.addEventListener("change", (evt) => {
+      updateFlag(evt.target);
+    });
   }
 })();
+
+const updateFlag = (element) => {
+  let currencyCode = element.value.toUpperCase();
+  let countryCode = flags[currencyCode];
+  let flag = countryCode.toLowerCase();
+  let newSrc = `https://flagcdn.com/64x48/${flag}.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", async (evt) => {
+  evt.preventDefault();
+  let amount = document.querySelector("form input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+  console.log(fromCurr.value, toCurr.value);
+  const URL = `${BASE_URL}/${fromCurr.value}.json`;
+  const response = await fetch(URL);
+  const data = await response.json();
+  console.log(data);
+});
